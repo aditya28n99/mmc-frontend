836a5b57b0626c1eddc16395c78e5e9a386ef82a
Aditya Sunil Wakale <adityawakale208@gmail.com>
2024-07-27 20:19:51 +0530

    applicaion and notification pages added

---
+++ src/pages/applications/applicatonsList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function applicatonsList() {
+  return (
+    <div>applicatons List</div>
+  )
+}
+
+export default applicatonsList
---
+++ src/pages/notifications/allNotifications.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function allNotifications() {
+  return (
+    <div>allNotifications</div>
+  )
+}
+
+export default allNotifications
